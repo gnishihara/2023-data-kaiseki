d851e78103a6a34721f572ac194b1a8cfa6a827a
Greg Nishihara <gnishihara@gmail.com>
2023-09-27 09:45:25 +0900

    update

--- script_ctd.R
+++ script_ctd.R
@@ -23,13 +23,42 @@ ctd01 = ctd01 |>
   select(depth = `水深 (m)`,
          temperature = `水温 (℃)` ,
          salinity = `塩分濃度 (PSS)`)
+
 # ステーションの情報と観測者のIDも追加する
 ctd01 = ctd01 |> 
   mutate(station = "st1",
          id = "A",
          date = "230926")
 
- 
+# 作業の効率化
+
+## ファイル名からステーション、観測者ID、日付を抽出する
+tmp = basename(filename)
+station = str_extract(tmp, "st[0-9]")
+id = str_extract(tmp, "_[A-Z]_") |> str_extract("[A-Z]")
+date = str_extract(tmp, "[0-9]{6}")
+
+
+# データ読み込み関数
+read_ctd_data = function(filename) {
+  tmp = basename(filename)
+  station = str_extract(tmp, "st[0-9]")
+  id = str_extract(tmp, "_[A-Z]_") |> str_extract("[A-Z]")
+  date = str_extract(tmp, "[0-9]{6}")
+  
+  ctd01 = read_csv(file = filename, skip = 28)
+  ctd01 = ctd01 |> 
+    select(depth = `水深 (m)`,
+           temperature = `水温 (℃)` ,
+           salinity = `塩分濃度 (PSS)`)
+  ctd01 |> 
+    mutate(station = station,
+           id = id,
+           date = date)
+}
+
+
+
 
 # rooturl = "https://github.com/gnishihara/2023-data-kaiseki/blob/main/data/"
 # f1 = "CTD_Rseminer_st1_A_230926.csv"
