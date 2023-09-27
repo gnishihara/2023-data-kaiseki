30623755e4a891626c507a0de88dd56367ddb214
Greg Nishihara <gnishihara@gmail.com>
2023-09-27 13:37:08 +0900

    First commit

--- script_multiple_comparisons.R
+++ script_multiple_comparisons.R
@@ -10,3 +10,32 @@ library(lemon)  # 作図用
 
 library(emmeans) # 多重比較用のパッケージ
 
+irist = iris |> as_tibble()
+
+# 変数名を修正
+irist = irist |> 
+  select(SL = Sepal.Length,
+         SW = Sepal.Width,
+         PL = Petal.Length,
+         PW = Petal.Width,
+         Species)
+
+# irist をピボットして、その結果を
+# 直接 ggplot() に渡す。
+irist |> 
+  pivot_longer(
+    cols = c("SL", "SW", "PL", "PW")
+  ) |> 
+  ggplot() +
+  geom_boxplot(
+    aes(
+      x = Species,
+      y = value
+    )
+  ) +
+  facet_rep_wrap(vars(name),
+                 nrow = 2)
+  
+  
+  
+  
