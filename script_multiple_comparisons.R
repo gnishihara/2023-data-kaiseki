04f2910799b205de8ff6ed4b5f511c7130a250ef
Greg Nishihara <gnishihara@gmail.com>
2023-09-27 13:57:01 +0900

    Add t-test

--- script_multiple_comparisons.R
+++ script_multiple_comparisons.R
@@ -36,6 +36,54 @@ irist |>
   facet_rep_wrap(vars(name),
                  nrow = 2)
   
-  
+irist |> 
+  pivot_longer(
+    cols = c("SL", "SW", "PL", "PW")
+  ) |> 
+  ggplot() +
+  geom_point(
+    aes(
+      x = Species,
+      y = value
+    ),
+    position = position_jitter(width = 0.2),
+    alpha = 0.2
+  ) +
+  facet_rep_wrap(vars(name),
+                 nrow = 2)
+
+
+# 多重比較
+# 作業仮説：種ごとのPLの長さが違う
+# 帰無仮説：種ごとのPLの平均値に違いがない
+# Null Hypothesis: There are no 
+# differences in the mean PL among species
+
+# virginica と versicolor のt検定
+irist |> 
+  filter(!str_detect(Species, "setosa")) |> 
+  t.test(PL ~ Species, data = _)
+
+# versicolor と setosa のt検定
+irist |> 
+  filter(!str_detect(Species, "virginca")) |> 
+  t.test(PL ~ Species, data = _)
+
+# virginica と setosa のt検定
+irist |> 
+  filter(!str_detect(Species, "versicolor")) |> 
+  t.test(PL ~ Species, data = _)
+
+
+
+
+
+
+
+
+
+
+
+
   
   
