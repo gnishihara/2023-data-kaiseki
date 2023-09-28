f967b58589569235fbccd8437edc79997f4de963
Greg Nishihara <gnishihara@gmail.com>
2023-09-28 14:25:40 +0900

    Update

--- script_gam.R
+++ script_gam.R
@@ -3,11 +3,14 @@
 # 2023 09 28
 # Greg Nishihara
 
-library(tidyverse)
-library(ggpubr)
-library(mgcv)
-library(lemon)
-library(statmod)
+library(tidyverse) # データ処理・作図用
+library(ggpubr)    # 作図用
+library(lemon)     # 作図用
+
+library(gratia) # GAM用パッケージ
+library(mgcv)   # GAM用パッケージ
+
+library(statmod) # ランダム化残差用
 
 dset = CO2 |> as_tibble() 
 dset
